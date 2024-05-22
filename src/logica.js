975f31a3e9ace37371658800f8eb3b4dd02d062a
JulianRengifo <jurenco1997@gmail.com>
2024-05-21 23:42:50 -0500

    se corrige la funcion iniciar
    -El juago ahora inicia con su repectivo boton. - function guardar se le añade para limpiar el textarea y se devuelve la variable finalizado a su estado determinado luego de activarla

--- src/logica.js
+++ src/logica.js
@@ -1,12 +1,14 @@
     
-    var palabras = [
+    let palabras = [
         "ALURA", "ORACLE", "FRONTEND", "BACKEND", "JAVA", "CODIGO", "ERROR", "DUBSTEP"
     ]
 
     // variable para almacenar la configuracion actual
-    var juego = null
+    let juego = null
+    // Verifica si ya se ha enviado alguna alerta
+    let finalizado = false
 
-    var $html ={
+    let $html ={
         hombre: document.getElementById("hombre"),
         adivinado: document.querySelector("#adivinado"),
         errado: document.querySelector("#errado")
@@ -14,10 +16,10 @@
 
     function dibujar(juego) {
         //Actualizar la imagen del muñeco
-        var $elem
+        let $elem
         $elem = $html.hombre
 
-        var estado = juego.estado
+        let estado = juego.estado
         if (estado === 8){        // Recicla la imagen de un estado anterior 
             estado = juego.previo
         }
@@ -25,8 +27,8 @@
         $elem.src = "images/estado" + estado + ".png";
 
         // Creamos las letras adivinadas
-        var palabra = juego.palabra       // Obtiene la palabra del objeto 'juego'
-        var adivinado = juego.adivinado
+        let palabra = juego.palabra       // Obtiene la palabra del objeto 'juego'
+        let adivinado = juego.adivinado
         $elem = $html.adivinado
         $elem.innerHTML = ""       // Borramos los elementos anteriores
         for (let letra of palabra) {   // Itera sobre cada letra de la palabra
@@ -43,7 +45,7 @@
         }
 
         // Creamos las letras erradas
-        var errado = juego.errado
+        let errado = juego.errado
         $elem = $html.errado
         $elem.innerHTML = ""       // Borramos los elementos anteriores
         for (let letra of errado) {    // Recorremos las letras que se han errado
@@ -56,21 +58,21 @@
     }
 
     function adivinar (juego, letra){
-        var estado = juego.estado
+        let estado = juego.estado
         // si se gana o pierde, termina el juego
         if ( estado === 1 || estado === 8){
             return
         }
 
-        var adivinado = juego.adivinado
-        var errado = juego.errado
+        let adivinado = juego.adivinado
+        let errado = juego.errado
         // si se a adivinado o errado la letra 
         if(adivinado.has(letra) || errado.has(letra)){ // has nos indica si pertenece al conjunto adivinado o errado
             return
         }
 
-        var palabra = juego.palabra
-        var letras = juego.letras
+        let palabra = juego.palabra
+        let letras = juego.letras
         // Si la letra ingresada hace parte de la pabra
         if(letras.has(letra)){
             adivinado.add(letra)  // Agregamos a la lista de letras adivinadas
@@ -93,36 +95,40 @@
     // adivinar la letra presionada utilizando una función llamada adivinar. Después de intentar adivinar la letra, actualiza la representación visual del juego llamando a la función dibujar.
 
     window.onkeypress = function adivinarletra(e){ //se ejecutará cuando se presione una tecla. Toma un parámetro e
-        var letra = e.key  //Obtiene la tecla presionada del objeto de evento e y la guarda en la variable letra.
+        if (!juego) return; // Evita que el programa reaccione a pulsaciones de teclas innecesarias cuando no hay un juego en curso
+        let letra = e.key  //Obtiene la tecla presionada del objeto de evento e y la guarda en la letiable letra.
         letra = letra.toUpperCase() //Convierte la tecla presionada a mayúscula
         if(/[^A-ZÑ]/.test(letra)){  // Verifica que no se ingrese un caracter erroneo
             return
         }
         adivinar(juego, letra)
-        var estado = juego.estado
-        if(estado === 8){
+        let estado = juego.estado
+        if(estado === 8 && !finalizado){
             setTimeout(function(){  // Esta función vacía no realiza ninguna operación específica, simplemente se ejecuta después de que haya transcurrido el tiempo especificado por setTimeout
                 alert("Felicidades ganaste")
+                finalizado = true   // muestra el mensaje cuado llega a este estado evitando repeticiones de alert
             }, 500)   // Espera medio segundo antes de mostrar la alerta
-        }else if(estado ===1){
+        }else if(estado ===1 && !finalizado){
             let palabra = juego.palabra
             setTimeout(function() {
                 alert("Lo siento, perdiste... la palabra era: " + palabra);
+                finalizado = true
             }, 500);
         }
         dibujar(juego) // Al cambiar el estado por la letras ingresadas se debe volver a dibujar el muñeco
     }
 
     window.nuevoJuego = function nuevoJuego(){
-        var palabra = palabraAleatoria()
-        juego = {}
+        let palabra = palabraAleatoria()
+        juego = {}              // establece la variable juego como un objeto vacío para comenzar a almacenar los detalles del juego mientras progresa.
         juego.palabra = palabra
         juego.estado = 7
         juego.adivinado = new Set()
         juego.errado = new Set()
+        finalizado = false     // vuleve la varialble determinada en false
 
         // Devuelve las letras que contiene la palabra
-        var letras = new Set()
+        let letras = new Set()
         for(let letra of palabra){
             letras.add(letra)
         }
@@ -134,13 +140,15 @@
     }
 
     function palabraAleatoria(){
-        var index = Math.trunc(Math.random() * palabras.length);
+        let index = Math.trunc(Math.random() * palabras.length);
         return palabras[index];
     }
 
-    nuevoJuego()
+    
 
     function iniciar(){
+
+        nuevoJuego();
 
     document.getElementById('agregar').style.display = 'none';
     document.getElementById('iniciar').style.display = 'none';
@@ -154,6 +162,9 @@
 }
 
 function desistir(){
+
+    juego = null; // reinicia el objeto juego
+
     document.getElementById('nuevo').style.display = "none";
     document.getElementById('desistir').style.display = "none";
     document.getElementById('hombre').style.display = "none";
@@ -182,13 +193,16 @@ function agregarPalabra() {
 function guardarPalabra() {
     
     // Obtener el valor del textarea
-    var palabraGuardada = document.getElementById("ingresePalabra").value;
+    let palabraGuardada = document.getElementById("ingresePalabra").value;
 
     // Verificar que la palabra cumpla con ciertas condiciones (mayúscula y de 8 caracteres)
     if (palabraGuardada.length > 8 || palabraGuardada !== palabraGuardada.toUpperCase()) {
         alert("Por favor, ingresa una palabra de 8 o menos caracteres en mayúsculas.");
         return; // Detener la ejecución si la palabra no cumple con las condiciones
     };
+
+     // Limpiar el textarea
+    document.getElementById("ingresePalabra").value = "";
 
     document.getElementById('guardar').style.display = "none";
     document.getElementById('cancelar').style.display = "none";
@@ -202,19 +216,19 @@ function guardarPalabra() {
     document.getElementById('adivinado').style.display = "inline-block";
     document.getElementById('errado').style.display = "inline-block";
 
-    var letras = new Set()
+    let letras = new Set()
         for(let letra of palabraGuardada){
             letras.add(letra)
         }
     
-    juego = {
-        palabra: palabraGuardada,
-        estado: 7, // Estado inicial del juego
-        adivinado: new Set(), // Conjunto para almacenar las letras adivinadas
-        errado: new Set(), // Conjunto para almacenar las letras erradas
-        letras: letras, // Conjunto para almacenar todas las letras de la palabra
-        restante: letras.size // Número de letras restantes por adivinar
-    };
+        juego = {}
+        juego.palabra = palabraGuardada,
+        juego.estado = 7, // Estado inicial del juego
+        juego.adivinado = new Set(), // Conjunto para almacenar las letras adivinadas
+        juego.errado = new Set(), // Conjunto para almacenar las letras erradas
+        juego.letras = letras, // Conjunto para almacenar todas las letras de la palabra
+        juego.restante = letras.size, // Número de letras restantes por adivinar
+        finalizado = false
 
     dibujar(juego);
     console.log("Palabra guardada:", palabraGuardada);
